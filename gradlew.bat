148fe02b7f3e3114e8a88332383e095542022d22
HiImRodney <thecaterbot@gmail.com>
2025-04-04 14:29:53 +0100

    Create gradlew.bat

---
+++ gradlew.bat
@@ -0,0 +1,14 @@
+**Path:** `gradlew.bat` (root level)
+```bat
+@echo off
+
+rem ----------------------------------------------------------------------------
+rem Gradle start up script for Windows
+rem ----------------------------------------------------------------------------
+
+set DIR=%~dp0
+
+"%DIR%gradle\wrapper\gradle-wrapper.jar" %*
+```
+
+---
